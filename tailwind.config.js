f1e79bb7f351c87d3c435fb79588e5340b88840b
prince.singh <prince.singh@xebia.com>
2022-12-05 12:56:49 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'salam-black': '#081F33',
+      },
+    },
+  },
+  plugins: [],
+};
